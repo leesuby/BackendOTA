9a18dfe8a43a9f6ecd90ad5bb41dde5ea5936ec7
19127203 <64020154+19127203@users.noreply.github.com>
2021-08-27 01:02:58 +0700

    update PORT using env

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ db.connect((err) => {
 });
 
 const app = express();
-
-app.listen('3000',()=>{
-    console.log('Server started on port 3000')
+const PORT=process.env.PORT ||3000;
+app.listen(PORT,()=>{
+    console.log('Server started on port',PORT)
 });
