1803965fa5814654c00f86cc17603e3ee511aec6
19127203 <64020154+19127203@users.noreply.github.com>
2021-08-31 12:05:03 +0700

    Update db.js

--- app/services/db.js
+++ app/services/db.js
@@ -18,7 +18,7 @@ function handleDisconnect() {
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }
-    console.log('connect database successfully!!',                                  // to avoid a hot loop, and to allow our node script to
+    console.log('connect database successfully!!',);                                  // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
